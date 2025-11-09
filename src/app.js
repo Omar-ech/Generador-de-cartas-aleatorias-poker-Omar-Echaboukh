8f0b87463a11bfc8267dfce9a0d4627cad2765b2
Omar Echaboukh <oechabouk@gmail.com>
2025-11-09 01:43:54 +0000

    actuliazado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function generarCarta() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ['♥️', '♦️', '♠️', '♣️'];
+  const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const cartaElemento = document.getElementById("carta-poker");
+  const paloTopElemento = document.getElementById("palo-arriba");
+  const paloBottomElemento = document.getElementById("palo-abajo");
+  const valorElemento = document.getElementById("valor-central");
+
+  function obtenerAleatorio(array) {
+    const indice = Math.floor(Math.random() * array.length);
+    return array[indice];
+  }
+
+  const paloElegido = obtenerAleatorio(palos);
+  const valorElegido = obtenerAleatorio(valores);
+
+  let colorClase = '';
+
+  if (paloElegido === '♥️' || paloElegido === '♦️') {
+    colorClase = "red";
+  } else {
+    colorClase = "black";
+  }
+
+  cartaElemento.classList.remove('red', 'black');
+  cartaElemento.classList.add(colorClase);
+
+  paloTopElemento.textContent = paloElegido;
+  paloBottomElemento.textContent = paloElegido;
+  paloTopElemento.className = `suit top ${colorClase}`;
+  paloBottomElemento.className = `suit bottom ${colorClase}`;
+
+  valorElemento.textContent = valorElegido;
+
+  valorElemento.classList.remove('red', 'black');
+  valorElemento.classList.add(colorClase);
+
+  const botonGenerar = document.getElementById('boton-generador');
+  if (botonGenerar) {
+    botonGenerar.addEventListener('click', generarCarta);
+  }
 };
